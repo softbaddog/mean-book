0074518dab5834588e09a578d098c2f0f3d3145c
Fu Wei <softbaddog@gmail.com>
2016-07-30 17:26:03 +0800

    修正bugs

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -7,7 +7,6 @@ exports.render = function(req, res) {
 
 	res.render('index', {
 		title: 'Hello World',
-		userFullName: req.user ? req.user.fullName : '',
 		user: JSON.stringify(req.user)
 	});
 };
